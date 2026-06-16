68e0b31a25788d050ed63f3de467116cc738c30b
qkrwnstj1346 <qkrwnstj1346@gmail.com>
2026-06-16 20:02:27 +0900

    param data 네이밍 작업

--- src/main/java/com/vita/vitapickBack/admin/controller/AdminCsInquiriesController.java
+++ src/main/java/com/vita/vitapickBack/admin/controller/AdminCsInquiriesController.java
@@ -23,13 +23,13 @@ public class AdminCsInquiriesController {
 
     @GetMapping
     public ResponseEntity<AdminCsInquiriesResponseDTO> getInquiries(
-            @RequestParam(defaultValue = "0") int page,
-            @RequestParam(defaultValue = "10") int size,
-            @RequestParam(required = false) String keyword,
-            @RequestParam(required = false) String status,
-            @RequestParam(required = false) String type,
-            @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate startDate,
-            @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate endDate) {
+            @RequestParam(name = "page", defaultValue = "0") int page,
+            @RequestParam(name = "size", defaultValue = "10") int size,
+            @RequestParam(name = "keyword", required = false) String keyword,
+            @RequestParam(name = "status", required = false) String status,
+            @RequestParam(name = "type", required = false) String type,
+            @RequestParam(name = "startDate", required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate startDate,
+            @RequestParam(name = "endDate", required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate endDate) {
 
         return ResponseEntity.ok(adminCsInquiriesService.getInquiries(page, size, keyword, status, type, startDate, endDate));
     }
